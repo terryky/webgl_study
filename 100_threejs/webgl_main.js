ae43c00e0ad86fe92d94d64fee98dc4d3e94d5f7
terryky <terryky1220@gmail.com>
2023-01-28 13:07:15 +0900

    added 100_threejs

---
+++ 100_threejs/webgl_main.js
@@ -0,0 +1,53 @@
+/* ---------------------------------------------------------------- *
+ *      M A I N    F U N C T I O N
+ * ---------------------------------------------------------------- */
+function startWebGL()
+{
+    const width  = 640;
+    const height = 480;
+
+    /* renderer */
+    const renderer = new THREE.WebGLRenderer({
+        alpha : false,
+        canvas: document.querySelector('#glcanvas')
+    });
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    const scene  = new THREE.Scene();
+
+
+    /* camera */
+    const camera = new THREE.PerspectiveCamera(72.0, width / height, 1.0, 1000.0);
+    camera.position.set(0, 0, 3.5);
+
+
+    /* light */
+    const light = new THREE.PointLight(0xffffff);
+    light.position.set(5, 1, 5);
+    scene.add(light);
+
+
+    /* geometry */
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
+    const box = new THREE.Mesh(geometry, material);
+    scene.add(box);
+
+
+    /* ----------------------- *
+     *  main loop
+     * ----------------------- */
+    render();
+
+    function render()
+    {
+        box.rotation.x = THREE.Math.DEG2RAD * 30;
+        box.rotation.y += 0.01;
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render);
+    }
+}
+
