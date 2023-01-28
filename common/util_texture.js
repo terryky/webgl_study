aef2ca8124aab57024b42230780a25543c055caf
terryky <terryky1220@gmail.com>
2023-01-28 20:22:36 +0900

    initialize gl.UNPACK_FLIP_Y_WEBGL to false

--- common/util_texture.js
+++ common/util_texture.js
@@ -29,6 +29,7 @@ GLUtil.create_image_texture = function (gl, url)
     teximage.onload = function ()
     {
         gl.bindTexture(gl.TEXTURE_2D, texid);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
         gl.texImage2D (gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, teximage);
         gl.generateMipmap (gl.TEXTURE_2D);
     }
