ca175379692baa524faeb3266a32f2afaaa80789
terryky <terryky1220@gmail.com>
2024-01-04 22:23:32 +0900

    refactoring

--- webgl2/common/glutil.js
+++ webgl2/common/glutil.js
@@ -1,5 +1,7 @@
 export {Shader}     from "./glutil_shader.js"
 export {Texture}    from "./glutil_texture.js"
-
+export {Matrix}     from "./glutil_matrix.js"
+export {dbgstr}     from "./glutil_debugstr.js"
+export {pmeter}     from "./glutil_pmeter.js"
 
 
